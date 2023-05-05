9ebc7e492da200191f88ea70a997436192006509
Matthis Gördel <matthis@posteo.de>
2023-05-05 23:45:26 +0200

    initialize service repo from snamel

---
+++ service/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+    ]
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          ocamlPkgs = pkgs.ocamlPackages;
+        in
+        {
+          packages = {
+            default = ocamlPkgs.buildDunePackage {
+              pname = "yvm";
+              version = builtins.substring 0 8 self.lastModifiedDate;
+              duneVersion = "3";
+              nativeBuildInputs = [
+              ];
+              src = ./.;
+            };
+          };
+
+          apps.default = utils.lib.mkApp { drv = self.packages.${system}.default; };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nixpkgs-fmt
+              ocamlformat
+              ocamlPackages.ocaml-lsp
+            ];
+
+            inputsFrom = [ self.packages.${system}.default ];
+          };
+
+        });
+}
