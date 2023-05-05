fc09cc73a888c3efcea6256824d9a66112100e7f
Matthis Gördel <matthis@posteo.de>
2023-05-06 00:39:43 +0200

    first types from spec

--- service/yvm.ml
+++ service/yvm.ml
@@ -1,1 +1,14 @@
-print_endline "hello world"
+type primType =
+  | Boolean of int
+  | Byte of int
+  | Short of int
+  | Int of int32
+  | Long of int64
+  | Char of char
+  | Float of float
+  | Double of float
+  | Reference
+  | ReturnAddress
+
+type frame = { locals : primType list; operand_stack : primType list }
+type foo = { pc : int; stack : frame list }
