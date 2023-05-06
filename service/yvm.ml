1eb3c9ba4d38fa1c17820888ae5816fa11314f73
Matthis Gördel <matthis@posteo.de>
2023-05-06 17:14:09 +0200

    weiter constant_pool parsen

--- service/yvm.ml
+++ service/yvm.ml
@@ -11,28 +11,30 @@ type primType =
   | ReturnAddress
 
 type constant =
-  | C_Utf8
+  | C_Utf8 of string
   | C_Integer
   | C_Float
   | C_Long
   | C_Double
-  | C_Class
+  | C_Class of { name_index : int }
   | C_String
-  | C_Fieldref
+  | C_Fieldref of { class_index : int; name_and_type_index : int }
   | C_Methodref of { class_index : int; name_and_type_index : int }
-  | C_InterfaceMethodref
-  | C_NameAndType
+  | C_InterfaceMethodref of { class_index : int; name_and_type_index : int }
+  | C_NameAndType of { name_index : int; descriptor_index : int }
   | C_MethodHandle
   | C_MethodType
   | C_Dynamic
   | C_InvokeDynamic
   | C_Module
   | C_Package
+[@@deriving show]
 
 type frame = { locals : primType list; operand_stack : primType list }
 type foo = { pc : int; stack : frame list }
 
 type jclass = { version : int * int; constant_pool : constant array }
+[@@deriving show]
 
 let input_u2 ic =
   let b1 = input_byte ic lsl 8 in
@@ -50,50 +52,63 @@ let input_u8 ic =
   u41 lor u42
 
 let read_class ic =
-  input_char ic |> Char.code |> Printf.printf "%x";
-  input_char ic |> Char.code |> Printf.printf "%x";
-  input_char ic |> Char.code |> Printf.printf "%x";
-  input_char ic |> Char.code |> Printf.printf "%x";
-  print_endline "";
+  assert (input_u4 ic = 0xcafebabe);
   let minor = input_u2 ic in
   let major = input_u2 ic in
-  Printf.printf "v%d.%d\n" major minor;
   let constant_pool_count = input_u2 ic in
-  print_int constant_pool_count;
-  print_endline "";
-  let kind = input_byte ic in
-  print_int kind;
-  print_endline "";
-  let constant_pool = Array.make constant_pool_count C_Utf8 in
-  for i = 0 to constant_pool_count - 1 do
+  let constant_pool = Array.make constant_pool_count (C_Utf8 "fill") in
+  for i = 1 to constant_pool_count - 1 do
+    let kind = input_byte ic in
+    print_int kind;
+    print_endline "";
     let c =
       match kind with
-      | 1 -> C_Utf8
+      | 1 ->
+          let length = input_u2 ic in
+          C_Utf8 (really_input_string ic length)
+      (*
       | 3 -> C_Integer
       | 4 -> C_Float
       | 5 -> C_Long
       | 6 -> C_Double
-      | 7 -> C_Class
       | 8 -> C_String
-      | 9 -> C_Fieldref
+         *)
+      | 7 -> C_Class { name_index = input_u2 ic }
+      | 9 ->
+          let class_index = input_u2 ic in
+          let name_and_type_index = input_u2 ic in
+          C_Fieldref { class_index; name_and_type_index }
       | 10 ->
-          C_Methodref
-            { class_index = input_u2 ic; name_and_type_index = input_u2 ic }
-      | 11 -> C_InterfaceMethodref
-      | 12 -> C_NameAndType
+          let class_index = input_u2 ic in
+          let name_and_type_index = input_u2 ic in
+          C_Methodref { class_index; name_and_type_index }
+      | 11 ->
+          let class_index = input_u2 ic in
+          let name_and_type_index = input_u2 ic in
+          C_InterfaceMethodref { class_index; name_and_type_index }
+      | 12 ->
+          let name_index = input_u2 ic in
+          let descriptor_index = input_u2 ic in
+          C_NameAndType { name_index; descriptor_index }
+      (*
       | 15 -> C_MethodHandle
       | 16 -> C_MethodType
       | 17 -> C_Dynamic
       | 18 -> C_InvokeDynamic
       | 19 -> C_Module
       | 20 -> C_Package
+         *)
       | _ -> raise (Failure "unexpected Constant Kind Tag")
     in
     constant_pool.(i) <- c
   done;
-  (*
-  show_jclass { version = (1, 1); constant_pool = constant_pool } |> print_endline;
-     *)
+  show_jclass { version = (major, minor); constant_pool } |> print_endline;
+  Array.iteri
+    (fun i el ->
+      print_int i;
+      print_char ' ';
+      show_constant el |> print_endline)
+    constant_pool;
   print_endline ""
 
 let () = In_channel.with_open_bin "Foo.class" read_class
