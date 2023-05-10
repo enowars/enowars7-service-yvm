e1951c3148c2cebcf5a1ce17a739e4883d5f5adc
Matthis Gördel <matthis@posteo.de>
2023-05-10 16:08:24 +0200

    move main to yvm.ml

---
+++ service/yvm.ml
@@ -0,0 +1,13 @@
+open Jparser
+
+let () =
+  let r_cls = parse_class "Foo.class" in
+  show_raw_class r_cls |> print_endline;
+  Array.iteri
+    (fun i el ->
+      print_int i;
+      print_char ' ';
+      show_constant el |> print_endline)
+    r_cls.constant_pool;
+  let c_cls = cook_class r_cls in
+  c_cls |> show_ckd_class |> print_endline
