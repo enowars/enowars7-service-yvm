5cb93bbd84505f95809506dafa69146384443930
Matthis Gördel <matthis@posteo.de>
2023-05-10 14:20:29 +0200

    interpret first opcode (frames etc missing)

--- service/yvm.ml
+++ service/yvm.ml
@@ -10,4 +10,5 @@ let () =
       show_constant el |> print_endline)
     r_cls.constant_pool;
   let c_cls = cook_class r_cls in
-  c_cls |> show_ckd_class |> print_endline
+  c_cls |> show_ckd_class |> print_endline;
+  Jinterpreter.run c_cls
