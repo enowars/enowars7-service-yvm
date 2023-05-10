8f9fa214748f3c6b20c20bf62f7534a5c7498070
Matthis Gördel <matthis@posteo.de>
2023-05-10 21:05:39 +0200

    fix dune stuff

--- service/src/yvm.ml
+++ service/src/yvm.ml
@@ -1,14 +1,14 @@
-open Jparser
+open Yvm_lib
 
 let () =
-  let r_cls = parse_class "Foo.class" in
-  show_raw_class r_cls |> print_endline;
+  let r_cls = Jparser.parse_class "Foo.class" in
+  Jparser.show_raw_class r_cls |> print_endline;
   Array.iteri
     (fun i el ->
       print_int i;
       print_char ' ';
-      show_constant el |> print_endline)
+      Jparser.show_constant el |> print_endline)
     r_cls.constant_pool;
-  let c_cls = cook_class r_cls in
-  c_cls |> show_ckd_class |> print_endline;
+  let c_cls = Jparser.cook_class r_cls in
+  c_cls |> Jparser.show_ckd_class |> print_endline;
   Jinterpreter.run c_cls
