62d9f826e87c19c1db1c38c91d0a19230735d222
Matthis Gördel <matthis@posteo.de>
2023-05-14 17:04:42 +0200

    parametrise run with method ref

--- service/src/yvm.ml
+++ service/src/yvm.ml
@@ -11,4 +11,4 @@ let () =
     r_cls.constant_pool;
   let c_cls = Jparser.cook_class r_cls in
   c_cls |> Jparser.show_ckd_class |> print_endline;
-  Jinterpreter.run c_cls
+  Jinterpreter.run c_cls ("main", "([Ljava/lang/String;)V")
