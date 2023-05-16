2e95954a04f2ca990aafb40df053d78e0fa5d4f3
Matthis Gördel <matthis@posteo.de>
2023-05-17 00:53:11 +0200

    enable replaying by handing uploader uniq id

--- service/runner.php
+++ service/runner.php
@@ -1,30 +1,61 @@
 <?php
 
+function run_file($filename) {
+  echo "<p>yvm output:</p>";
+
+  echo "<pre>";
+  echo shell_exec("cd classes; ../yvm $filename");
+  echo "</pre>";
+}
+
+const REPLAY_FILE = "classes/replay.tsv";
+const REPLAY_KEY = "replay_id";
+
+if (isset($_GET[REPLAY_KEY])) {
+  if (file_exists(REPLAY_FILE)) {
+    foreach (file(REPLAY_FILE) as $_ => $line) {
+      $e = explode(" ", $line);
+      if ($e[0] == $_GET[REPLAY_KEY]) {
+        run_file($e[1]);
+        die;
+      }
+    }
+  }
+  echo "<p>Sorry, unknown replay_id.</p>";
+  die;
+}
+
+if (!isset($_FILES["fileToUpload"])) {
+  echo "<p>Sorry, expecting file upload.</p>";
+  die;
+}
+
 $target_dir = "classes/";
 $filename = basename($_FILES["fileToUpload"]["name"]);
 $target_file = $target_dir . $filename;
-$uploadOk = 1;
+
+$regex = "/[A-Za-z]+\.class/";
+if (!preg_match($regex, $filename)) {
+  echo "<p>Sorry, expecting filename that matches $regex.</p>";
+  die;
+}
 
 // Check if file already exists
 if (file_exists($target_file)) {
   echo "<p>Sorry, file already exists.</p>";
-  $uploadOk = 0;
+  die;
 }
 
-if ($uploadOk == 0) {
-  echo "<p>Sorry, your file was not uploaded.</p>";
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "<p>The file ". htmlspecialchars($filename). " has been uploaded.</p>";
-
-    echo "<pre>";
-    // TODO make getflag work
-    echo shell_exec("cd classes; ../yvm " . $filename);
-    echo "</pre>";
-  } else {
-    echo "<p>Sorry, there was an error uploading your file.</p>";
-  }
+if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+  echo "<p>Sorry, there was an error uploading your file.</p>";
 }
+
+echo "<p>The file " . $filename . " has been uploaded.</p>";
+
+$id = uniqid();
+echo "<p>You can replay the file via the replay_id <a href='runner.php?" . REPLAY_KEY . "=$id'>$id</a>.</p>";
+
+file_put_contents(REPLAY_FILE, "$id $filename\n", FILE_APPEND | LOCK_EX);
+
+run_file($filename);
 ?>
-<br>
-<a href=".">go back.</a>
