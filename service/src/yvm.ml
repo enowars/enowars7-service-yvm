a902df328861af585179ba1b5127c09858b0cee9
Matthis Gördel <matthis@posteo.de>
2023-06-15 15:12:47 +0200

    don't print first loaded class

--- service/src/yvm.ml
+++ service/src/yvm.ml
@@ -9,13 +9,6 @@ let () =
     | _ -> failwith "expecting a single class file as arg"
   in
   let r_cls = Jparser.parse_class klass in
-  Jparser.show_raw_class r_cls |> print_endline;
-  Array.iteri
-    (fun i el ->
-      print_int i;
-      print_char ' ';
-      Jparser.show_constant el |> print_endline)
-    r_cls.constant_pool;
   let c_cls = Jparser.cook_class r_cls in
   (* TODO call <clinit> of main class *)
   Jinterpreter.run c_cls ("main", "([Ljava/lang/String;)V")
