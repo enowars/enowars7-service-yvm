7e9a927d8afba235e2a9694ead24b256db46212a
Matthis Gördel <matthis@posteo.de>
2023-07-01 23:29:57 +0200

    format java test files

--- service/test/interpreter/array_multidim.t/Foo.java
+++ service/test/interpreter/array_multidim.t/Foo.java
@@ -7,8 +7,8 @@ class Foo {
 		char[][] x0 = new char[2][];
 		x[0] = x0;
 
-		char[] x00 = {'n', 'a'};
-		char[] x01 = {'i', 's'};
+		char[] x00 = { 'n', 'a' };
+		char[] x01 = { 'i', 's' };
 		x0[0] = x00;
 		x0[1] = x01;
 
