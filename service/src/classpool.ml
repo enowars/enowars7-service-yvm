d7e64c0c68b3bacbc4612dbc6e601b704a117d85
Matthis Gördel <matthis@posteo.de>
2023-07-02 21:07:48 +0200

    refactor non-main <clinit> handling

--- service/src/classpool.ml
+++ service/src/classpool.ml
@@ -2,43 +2,45 @@ type pool_entry = Method of Jparser.meth | Int of int
 type klass = Jparser.ckd_class
 type t = (string * klass) list ref
 
-let rec get_field run_callback (t : t) (caller : string) (klass : string)
+let rec get_field (t : t) (caller : string) (klass : string)
     (nat : string * string) =
   match List.assoc_opt klass !t with
   | Some kpool -> (
       match List.assoc_opt nat kpool.fields with
-      | Some (_, entry) -> entry
+      | Some (_, entry) -> Ok entry
       | None -> failwith "invalid nat into loaded class")
   | None ->
       let ckd_cls =
         Jparser.parse_class (klass ^ ".class") |> Jparser.cook_class
       in
       t := (klass, ckd_cls) :: !t;
-      let () =
+      let r =
         match List.assoc_opt ("<clinit>", "()V") ckd_cls.meths with
-        | Some _ -> run_callback ckd_cls ("<clinit>", "()V")
-        | None -> ()
+        | Some (LocalMeth meth) -> Error (ckd_cls, meth)
+        | Some NativeMeth -> failwith "<clinit> shouldn't be native"
+        | None -> get_field t caller klass nat
       in
-      get_field run_callback t caller klass nat
+      r
 
-let rec get_method run_callback (t : t) (caller : string) (klass : string)
+let rec get_method (t : t) (caller : string) (klass : string)
     (nat : string * string) =
   match List.assoc_opt klass !t with
   | Some kpool -> (
       match List.assoc_opt nat kpool.meths with
-      | Some meth -> meth
+      | Some meth -> Ok meth
       | None -> failwith "invalid nat into loaded class")
   | None ->
       let ckd_cls =
         Jparser.parse_class (klass ^ ".class") |> Jparser.cook_class
       in
       t := (klass, ckd_cls) :: !t;
-      let () =
+      let r =
         match List.assoc_opt ("<clinit>", "()V") ckd_cls.meths with
-        | Some _ -> run_callback ckd_cls ("<clinit>", "()V")
-        | None -> ()
+        | Some (LocalMeth meth) -> Error (ckd_cls, meth)
+        | Some NativeMeth -> failwith "<clinit> shouldn't be native"
+        | None -> get_method t caller klass nat
       in
-      get_method run_callback t caller klass nat
+      r
 
 let show (t : t) =
   List.map
